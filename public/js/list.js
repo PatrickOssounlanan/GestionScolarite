4a1e8ab0e44efc4e45d77d1ec43c003a56a61dc4
root <root@localhost.localdomain>
2021-08-14 05:16:43 +0200

    folder student page program Devoir,Interrogation ; folder Teacher page listEtudiant folder censor page addEleve

---
+++ public/js/list.js
@@ -0,0 +1,10 @@
+function openForm() {
+    document.getElementById("form-modal").style.display="block";
+  }
+  
+  function closeForm() {
+    document.getElementById("form-modal").style.display="none";
+  }
+ document.getElementById("btn").addEventListener('click',(e)=>{
+      e.preventDefault();
+  })
